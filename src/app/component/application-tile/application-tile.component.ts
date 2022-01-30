e4e6c0546efe5983c0881e18cb6c1f7e1ca17312
Ruchi Agarwal <ruchismailbox@gmail.com>
2022-01-29 22:44:09 -0600

    Added application components

---
+++ src/app/component/application-tile/application-tile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-application-tile',
+  templateUrl: './application-tile.component.html',
+  styleUrls: ['./application-tile.component.scss']
+})
+export class ApplicationTileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
