03dcf70b5135aa2d419e3c36939dbfcc1587e851
Ruchi Agarwal <ruchismailbox@gmail.com>
2022-01-31 21:48:43 -0600

    login Component functionality

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { Subject } from 'rxjs';
 import { User } from 'src/app/model/user';
 import { LoginService } from 'src/app/services/login.service';
@@ -13,14 +14,25 @@ export class LoginComponent implements OnInit {
   password: string = '';
   searchSubject = new Subject();
   user!: User;
+  constructor(
+    private loginSvc: LoginService,
+    private formBuilder: FormBuilder
+  ) {}
 
-  constructor(private loginSvc: LoginService) {}
+  loginCred = this.formBuilder.group({
+    email: [''],
+    password: [''],
+  });
 
   ngOnInit(): void {
     this.loginSvc.init().subscribe((u) => (this.user = u));
   }
 
-  login(email: string, password: string) {
-    this.loginSvc.login(email, password);
+  login() {
+    console.log(this.loginCred.value);
+    this.loginSvc.login(
+      this.loginCred.value.email,
+      this.loginCred.value.password
+    );
   }
 }
