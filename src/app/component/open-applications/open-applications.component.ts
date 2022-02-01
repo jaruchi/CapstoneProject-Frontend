14535ad62be2e73bd9e72489d808f9d093d4ff9c
Ruchi Agarwal <ruchismailbox@gmail.com>
2022-01-31 21:52:02 -0600

    open applications

--- src/app/component/open-applications/open-applications.component.ts
+++ src/app/component/open-applications/open-applications.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Application } from 'src/app/model/application';
 import { openApplications } from 'src/app/model/mock';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-open-applications',
@@ -8,8 +9,12 @@ import { openApplications } from 'src/app/model/mock';
   styleUrls: ['./open-applications.component.scss'],
 })
 export class OpenApplicationsComponent implements OnInit {
-  applications: Application[] = [...openApplications];
-  constructor() {}
+  applications: Application[] = [];
+  constructor(private apiSvc: ApiService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.apiSvc
+      .getOpenApps()
+      .subscribe((openApps) => (this.applications = openApps));
+  }
 }
